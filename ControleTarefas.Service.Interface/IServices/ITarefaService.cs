d3ba65c23ad56f9efcce41ce3fb1cb5e0f9fa7b1
VictorZuccolotto <victor.zuccolotto@ufu.br>
2024-05-17 14:10:41 -0300

    #5 Criação da arquitetura utilizando o padrão de camadas com os conceitos do SOLID

---
+++ ControleTarefas.Service.Interface/IServices/ITarefaService.cs
@@ -0,0 +1,15 @@
+﻿using ControleTarefas.Entity.DTOs;
+using ControleTarefas.Entity.Entities;
+
+namespace ControleTarefas.Service.Interface.IServices
+{
+    public interface ITarefaService
+    {
+        public TarefaDTO Add(Tarefa novaTarefa);
+        public TarefaDTO Update(string titulo, Tarefa novaTarefa);
+        public TarefaDTO Delete(string titulo);
+        public TarefaDTO Get(string titulo);
+        public List<TarefaDTO> GetAll();
+
+    }
+}
---
+++ ControleTarefas.WebApi/Startup.cs
@@ -0,0 +1,55 @@
+﻿using ControleTarefas.Repository.Interface.IRepositories;
+using ControleTarefas.Repository.Repositories;
+using ControleTarefas.Service.Interface.IServices;
+using ControleTarefas.Service.Services;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+
+namespace ControleTarefas.Api
+{
+    public class Startup
+    {
+        public Startup()
+        {
+
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddScoped<ITarefaRepository, TarefaRepository>();
+            services.AddScoped<ITarefaService, TarefaService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.MapType(typeof(TimeSpan), () => new() { Type = "string", Example = new OpenApiString("00:00:00") });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Controle de Tarefas",
+                    Version = "v1",
+                    Description = "APIs de estudo"
+                });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Controle de tarefas v1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
