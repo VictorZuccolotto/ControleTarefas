3b39d4962e9d916abffc1d6d1a1945f0c1e5d3de
VictorZuccolotto <victor.zuccolotto@ufu.br>
2024-05-17 14:42:02 -0300

    Implementação do log4net

--- ControleTarefas.WebApi/Middleware/ApiMiddleware.cs
+++ ControleTarefas.WebApi/Middleware/ApiMiddleware.cs
@@ -2,21 +2,33 @@
 using ControleTarefas.Utils.Exceptions;
 using ControleTarefas.Utils.Messages;
 using ControleTarefas.Utils.Responses;
+using log4net;
 using Newtonsoft.Json;
+using System.Diagnostics;
 using System.Net;
 
 namespace ControleTarefas.WebApi.Middleware
 {
     public class ApiMiddleware : IMiddleware
     {
+
+        private readonly ILog _log = LogManager.GetLogger(typeof(Program));
+
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
             try
             {
                 await next(context);
-            }catch (Exception ex)
+                stopwatch.Stop();
+                _log.InfoFormat("Serviço executado com sucesso {0}, {1}, [{2} ms]", context.Request.Method, context.Request.Path, stopwatch.ElapsedMilliseconds);
+            }
+            catch (Exception ex)
             {
+                stopwatch.Stop();
                 await HandleException(context, ex);
+                _log.ErrorFormat("Erro no serviço [{0}] {1}, ({2} ms)\n[\n{3}\n]", context.Request.Method, context.Request.Path, stopwatch.ElapsedMilliseconds, ex);
             }
         }
 
