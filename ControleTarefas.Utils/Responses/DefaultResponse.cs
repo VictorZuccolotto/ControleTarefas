2ef0eb0c0d8efa9d3d2c28698d7e3e66bc6df2f8
VictorZuccolotto <victor.zuccolotto@ufu.br>
2024-05-17 14:32:21 -0300

    Implementação do middleware

---
+++ ControleTarefas.Utils/Responses/DefaultResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleTarefas.Utils.Responses
+{
+    public class DefaultResponse
+    {
+        public HttpStatusCode HttpStatus { get; set; }
+        public List<string> Messages { get; set; }
+        public object Data { get; set; }
+
+        public DefaultResponse(HttpStatusCode status, List<string> messages)
+        {
+            HttpStatus = status;
+            Messages = messages;
+            Data = DateTime.Now;
+        }
+    }
+}
---
+++ ControleTarefas.WebApi/Middleware/ApiMiddleware.cs
@@ -0,0 +1,49 @@
+﻿
+using ControleTarefas.Utils.Exceptions;
+using ControleTarefas.Utils.Messages;
+using ControleTarefas.Utils.Responses;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ControleTarefas.WebApi.Middleware
+{
+    public class ApiMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }catch (Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+
+        private async Task HandleException(HttpContext context, Exception ex)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            var messages = new List<string>();
+            HttpStatusCode statusCode;
+            switch (ex)
+            {
+                case BusinessException:
+                    messages.Add(ex.Message);
+                    statusCode = HttpStatusCode.Conflict;
+                    break;
+                case GenericException:
+                    messages.Add(ex.Message);
+                    statusCode = HttpStatusCode.NotFound;
+                    break;
+                default:
+                    messages.Add(InfraMessages.InternalServerError);
+                    statusCode = HttpStatusCode.InternalServerError;
+                    break;
+            }
+            response.StatusCode = (int)statusCode;
+            await response.WriteAsync(JsonConvert.SerializeObject(new DefaultResponse(statusCode, messages)));
+        }
+    }
+}
