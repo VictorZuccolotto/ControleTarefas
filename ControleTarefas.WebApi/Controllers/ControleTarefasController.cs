40ddfb1179fc8d15a6aefab7e209d28dcb969d1c
VictorZuccolotto <victor.zuccolotto@ufu.br>
2024-05-17 13:34:27 -0300

    Criação do controller

---
+++ ControleTarefas.WebApi/Controllers/ControleTarefasController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace ControleTarefas.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ControleTarefaController : ControllerBase
+    {
+
+        private static List<string> Tarefas { get; set; } = new() { new("Teste"), new("Instalação"), new("Configuração"), new("Criar Projeto"), new("Exercício Prático") };
+
+        [HttpGet("ListarTodasTarefas")]
+        public ActionResult<List<string>> Get()
+        {
+            return Tarefas;
+        }
+
+        [HttpGet("ListarTarefa")]
+        public ActionResult<string> Get(string nome)
+        {
+            var tarefa = Tarefas.IndexOf(nome);
+            if (tarefa == -1)
+                return NotFound();
+            return Tarefas[tarefa];
+        }
+
+        [HttpPost("InserirTarefa")]
+        public ActionResult<string> Post(string novaTarefa)
+        {
+            var tarefa = Tarefas.IndexOf(novaTarefa);
+            if (tarefa != -1)
+                return Conflict();
+            Tarefas.Add(novaTarefa);
+            return Created("",novaTarefa);
+
+        }
+
+        [HttpDelete("DeletarTarefa")]
+        public ActionResult<string> Delete(string nomeTarefa)
+        {
+            var tarefa = Tarefas.IndexOf(nomeTarefa);
+            if (tarefa == -1)
+              return NotFound();
+            Tarefas.Remove(nomeTarefa);
+            return nomeTarefa;
+        }
+
+        [HttpPut("AlterarTarefa")]
+        public ActionResult<string> Put(string nomeTarefa, string novaTarefa)
+        {
+            var index = Tarefas.IndexOf(nomeTarefa);
+            if (index == -1)
+                return NotFound();
+            Tarefas[index] = novaTarefa;
+            return novaTarefa;
+        }
+    }
+}
